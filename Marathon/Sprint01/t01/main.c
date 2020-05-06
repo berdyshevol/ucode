19d839cf82670e856c0407055093bd61127c8c0b
berdyshevol <berdyshevo@gmail.com>
2020-05-06 22:19:47 +0300

    ucode

--- Marathon/Sprint01/t01/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void mx_write_knock_knock(void);
-
-int main(void) {
-    mx_write_knock_knock();
-    return 0;
-}
--- Marathon/Sprint01/t01/mx_write_knock_knock.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void mx_write_knock_knock(void) {
-    printf("Wake up, NEO \\ (^_^) / ...\n");
-    printf("The Matrix has you ...\n");
-}
